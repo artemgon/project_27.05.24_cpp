c1c8bbcabf1ab35bc8a1e0d2966ea0e81fa1f2bb
artemgon <artemhoncharuk123@gmail.com>
2024-05-27 20:16:35 +0300

    Creating the fifth task.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int myFunction(int x) {
-	return x * x * x;
+int myFunction(int x, int y) {
+	if (x > y) {
+		cout << "The first number is bigger";
+	}
+	else {
+		cout << "The second number is bigger";
+	}
+	return 0;
 }
 
 int main() {
-	cout << myFunction(3);
+	cout << myFunction(3, 10);
 	return 0;
 }
