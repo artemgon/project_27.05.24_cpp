57602bbfa9a5af4102d69e9993205397f12255a9
artemgon <artemhoncharuk123@gmail.com>
2024-05-27 20:11:20 +0300

    Creating the third task.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int myFunction(int x){
+	if ( (x % 2 == 0 || x % 3 == 0 || x % 5) && x != 2 && x != 3) {
+		cout << "The number isn't prime";
+	}
+	else {
+		cout << "The number is prime";
+	}
+	return 0;
+}
+
 int main() {
+	cout << myFunction(10);
 	return 0;
 }
