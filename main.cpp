07f5e28ee56bba8986f9fc347136a88994486564
artemgon <artemhoncharuk123@gmail.com>
2024-05-27 20:38:13 +0300

    Creating the first task of homework.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int myFunction(int x, int y) {
+	int power = 1;
+	for (int i = 1; i <= y; i++)
+	{
+		power = power * x;
+	}
+	return power;
+}
+
 int main() {
+	cout << myFunction(2, 5);
 	return 0;
 }
