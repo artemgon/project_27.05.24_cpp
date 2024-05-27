294407fabf24ab46cbf782742e32741f0860d2e4
artemgon <artemhoncharuk123@gmail.com>
2024-05-27 19:15:08 +0300

    Creating the first task.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
+void myFunction(int N, int K) {
+	for (int i = 0; i < N; i++)
+	{
+		cout << ".";
+	}
+	for (int i = 0; i < K; i++)
+	{
+		cout << "\n.";
+		for (int j = 0; j < N - 2; j++)
+		{
+			cout << " ";
+		}
+		cout << "." << endl;
+	}
+	for (int i = 0; i < N; i++)
+	{
+		cout << ".";
+	}
+}
+
 int main() {
+	myFunction(30, 10);
 	return 0;
 }
