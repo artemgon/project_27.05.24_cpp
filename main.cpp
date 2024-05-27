74bb61b8687f9a86135f992ff0db1e250ba69575
artemgon <artemhoncharuk123@gmail.com>
2024-05-27 20:22:01 +0300

    Creating the sixth task.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+bool myFunction(float x) {
+	if (x > 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
 int main() {
+	cout << myFunction(0);
 	return 0;
 }
