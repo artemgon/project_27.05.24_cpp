925b22fd75ac2aa5d9f65ff0e5d859ad198e1b22
artemgon <artemhoncharuk123@gmail.com>
2024-05-27 19:54:08 +0300

    Creating the second task.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int myFunction(int x) {
+	int product = 1;
+	for (int i = 1; i <= x; i++)
+	{
+		product = product * i;
+	}
+	return product;
+}
+
 int main() {
+	int variable = myFunction(5);
+	cout << variable;
 	return 0;
 }
