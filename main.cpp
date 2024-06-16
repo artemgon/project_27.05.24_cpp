fccdb2a56adbf85ae49f4df80fd73563b769f9e3
artemgon <artemhoncharuk123@gmail.com>
2024-06-16 09:15:41 +0300

    Creating the second task of hw.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
+int sum = 0;
+int SomeFunc(int start, int end)
+{
+	//First variant
+	for (int i = start + 1; i < end; i++)
+	{
+		sum += i;
+	}
+	//Second variant
+	//sum = ((start + 1) + (end - 1)) * ((end - 1) - (start+1) + 1) / 2; 
+	return sum;
+}
 
 int main() {
+	cout << SomeFunc(15, 30);
 	return 0;
 }
