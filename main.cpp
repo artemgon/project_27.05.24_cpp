9301e01107658e345b3efc1285d93665cf4ab0b5
artemgon <artemhoncharuk123@gmail.com>
2024-06-16 14:23:39 +0300

    Creating the fifth task of hw.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+void SomeFunc(int number)
+{
+	int sum_1 = 0, sum_2 = 0;
+	sum_1 = number % 10 + number / 10 % 10 + number / 100 % 10;
+	sum_2 = number / 1000 % 10 + number / 10000 % 10 + number / 100000 % 10;
+	if (sum_1 == sum_2) cout << "The number is happy";
+	else cout << "The number isn't happy";
+	return;
+}
+
 int main() {
+	SomeFunc(123123);
 	return 0;
 }
